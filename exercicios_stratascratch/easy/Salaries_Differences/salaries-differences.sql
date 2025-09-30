9bf9926aefd1cef59bc0d77ae287758ba4ca3d90
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2025-09-30 16:51:02 -0300

    Update salaries-differences.sql

--- exercicios_stratascratch/easy/Salaries_Differences/salaries-differences.sql
+++ exercicios_stratascratch/easy/Salaries_Differences/salaries-differences.sql
@@ -1,7 +1,28 @@
+-- Primeira alternativa de solução:
+
 SELECT 
     (SELECT MAX(salary) FROM db_employee WHERE department_id IN (4))
     -
     (SELECT MAX(salary) FROM db_employee WHERE department_id IN (1))
 AS Diferença_salario;
 
--- a diferença absoluta entre os maiores salários dos departamentos citados é de 2400.
+
+
+-- Segunda alternativa de solução: 
+
+SELECT ABS (MAX((CASE 
+            WHEN dept.department = 'marketing' THEN emp.salary
+            END
+        ))
+        -
+        MAX((CASE 
+            WHEN dept.department = 'engineering' THEN emp.salary
+            END
+        )))
+FROM db_employee emp
+JOIN db_dept dept ON emp.department_id = dept.id
+WHERE dept.department IN ('marketing', 'engineering');
+
+
+/* Conclusão: ambas resoluções retornam o mesmo valor de 2400, que é a diferença absoluta entre o maior salário de marketing e o maior salário de engenharia. 
+   A segunda query, porém, é mais exata, pois referencia diretamente o nome do departamento com o uso do JOIN.*/
