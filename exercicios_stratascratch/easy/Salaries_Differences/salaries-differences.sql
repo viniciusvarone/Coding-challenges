03094a247b596c7d0d568e7a75dfbd835a32611a
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2025-09-30 16:03:44 -0300

    Create salaries-differences.sql

---
+++ exercicios_stratascratch/easy/Salaries_Differences/salaries-differences.sql
@@ -0,0 +1,7 @@
+SELECT 
+    (SELECT MAX(salary) FROM db_employee WHERE department_id IN (4))
+    -
+    (SELECT MAX(salary) FROM db_employee WHERE department_id IN (1))
+AS Diferença_salario;
+
+-- a diferença absoluta entre os maiores salários dos departamentos citados é de 2400.
