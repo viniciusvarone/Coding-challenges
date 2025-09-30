c6751c2710b038fcfb63a97feefef8c7d24a587a
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2025-09-30 14:13:29 -0300

    Delete solucoes_stratascratch directory

--- solucoes_stratascratch/easy/Salaries Differences - ID 10308/salaries_differences.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    (SELECT MAX(salary) FROM db_employee WHERE department_id IN (4))
-    -
-    (SELECT MAX(salary) FROM db_employee WHERE department_id IN (1))
-AS Diferença_salario;
-
--- a diferença absoluta entre os maiores salários dos departamentos citados é de 2400.
